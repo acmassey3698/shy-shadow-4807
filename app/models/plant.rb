f6a5ea195cdaed5769a59647d999751252189429
Andrew Massey <acmassey@ncsu.edu>
2021-09-22 13:36:46 -0400

    Implement instance method instead of PlantPlot class method

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,4 +1,10 @@
 class Plant < ApplicationRecord
   has_many :plant_plots
   has_many :plots, through: :plant_plots
+
+  def find_plant_plot(plot_id)
+    plant_plots.where(plant_id: id)
+               .where(plot_id: plot_id)
+               .first
+  end 
 end
