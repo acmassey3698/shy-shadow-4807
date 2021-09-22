c634aea1ccb0a0fe371c6a9310614ca01adcecd1
Andrew Massey <acmassey@ncsu.edu>
2021-09-22 13:46:49 -0400

    Switch back to first solution for easier testing

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,16 +1,16 @@
 class Garden < ApplicationRecord
   has_many :plots
-  ## previous solution for referece
-  #has_many :plants, through: :plots
+  ## added for my current solution
+  has_many :plants, through: :plots
 
   def harvestable_in_hundred
-    plots.joins(:plants)
-       .select('plants.*')
-       .where('plants.days_to_harvest < ?', 100)
-       .distinct
+  ## Another solution. Don't know which is better...
+    # plots.joins(:plants)
+    #    .select('plants.*')
+    #    .where('plants.days_to_harvest < ?', 100)
+    #    .distinct
 
-  ## Previous solution for reference
-    # plants.where('days_to_harvest < ?', 100)
-    #       .distinct
+    plants.where('days_to_harvest < ?', 100)
+          .distinct
   end
 end
--- spec/models/garden_spec.rb
+++ spec/models/garden_spec.rb
@@ -4,4 +4,36 @@ RSpec.describe Garden do
   describe 'relationships' do
     it { should have_many(:plots) }
   end
+
+  before :each do
+    @garden = Garden.create!(name: "Turing Garden", organic: true)
+    @garden2 = Garden.create!(name: 'Andrews Garden', organic: false)
+
+    @plot1 = @garden.plots.create!(number: 1, size: 'small', direction: 'ENE' )
+    @plot2 = @garden.plots.create!(number: 2, size: 'small', direction: 'E')
+    @plot3 = @garden2.plots.create!(number: 3, size: 'medium', direction: 'SW')
+    @plot4 = @garden2.plots.create!(number: 4, size: 'large', direction: 'NW')
+
+    @plant1 = Plant.create(name: 'Peppers', description: 'sometimes its spicy', days_to_harvest: 50)
+    @plant2 = Plant.create(name: 'Tomato', description: 'Ew', days_to_harvest: 20)
+    @plant3 = Plant.create(name: 'Pumpkin', description: 'big and orange', days_to_harvest: 15)
+    @plant4 = Plant.create(name: 'Grapes', description: 'make some wine?', days_to_harvest: 60)
+
+    PlantPlot.create!(plant: @plant1, plot: @plot1)
+    PlantPlot.create!(plant: @plant2, plot: @plot1)
+    PlantPlot.create!(plant: @plant3, plot: @plot1)
+    PlantPlot.create!(plant: @plant4, plot: @plot2)
+    PlantPlot.create!(plant: @plant4, plot: @plot2)
+    PlantPlot.create!(plant: @plant1, plot: @plot3)
+    PlantPlot.create!(plant: @plant2, plot: @plot4)
+  end
+
+  describe 'instance methods' do
+    describe '#harvestable_in_hundred' do
+      it 'returns a distinct list of plants harvestable in less than 100 days' do
+        
+        expect(@garden.harvestable_in_hundred).to eq([@plant1, @plant2, @plant3, @plant4])
+      end
+    end
+  end
 end
