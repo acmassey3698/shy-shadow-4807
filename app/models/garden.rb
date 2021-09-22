48e1051c5ca73c1133728bf43595afd1b0d277d7
Andrew Massey <acmassey@ncsu.edu>
2021-09-22 13:22:08 -0400

    Add my previous solution to U-story 3, but commented out

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,10 +1,16 @@
 class Garden < ApplicationRecord
   has_many :plots
+  ## previous solution for referece
+  #has_many :plants, through: :plots
 
   def harvestable_in_hundred
-      plots.joins(:plants)
-         .select('plants.*')
-         .where('plants.days_to_harvest < 100')
-         .distinct
+    plots.joins(:plants)
+       .select('plants.*')
+       .where('plants.days_to_harvest < ?', 100)
+       .distinct
+
+  ## Previous solution for reference
+    # plants.where('days_to_harvest < ?', 100)
+    #       .distinct
   end
 end
